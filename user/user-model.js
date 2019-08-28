ba2a867590b3c08d89858153ad9d3d010c4260e7
Jamie Goodnight <goodnightlambda@gmail.com>
2019-08-28 04:07:23 -0400

    Testing user

--- user/user-model.js
+++ user/user-model.js
@@ -1,37 +1,35 @@
-const knex = require('knex')
-const knexConfig = require('../knexfile.js')
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
 
-const db = knex(knexConfig.development)
+const db = knex(knexConfig.development);
 
 module.exports = {
   findUserId,
   create,
   findUser,
   find
-}
+};
 
 function findUserId(id) {
-  return db('users')
+  return db("users")
     .where({ id })
-    .first()
+    .first();
 }
 
 function create(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(ids => {
-      const [id] = ids;
-      return findUserId(id)
-        .select('id', 'username', 'role_id')
-    })
+  return db("users");
+  // .insert(user, 'id')
+  // .then(ids => {
+  //   const [id] = ids;
+  //   return findUserId(id)
+  //     .select('id', 'username', 'role_id')
+  // })
 }
 
 function findUser(filter) {
-  return db('users')
-    .where(filter)
+  return db("users").where(filter);
 }
 
 function find() {
-  return db('users')
-    .select('id', 'username', 'role_id')
+  return db("users").select("id", "username", "role_id");
 }
